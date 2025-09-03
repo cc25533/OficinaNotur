aee5cccd06401a7c2570fea4ab6857a1f09a8203
cc25533 <cc25533@g.unicamp.br>
2025-09-03 20:24:33 -0300

    atualizei o main.py
    Fiz o que me mandaram

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ print ("ola mundo")
 
 lista = [1,2,3,4,5,6,7]
 
+def multiplicaLista(lista):
+    novalista = [3 * i for i in lista]
+    return novalista
 
-novalista = [3 * i for i in lista]
-
-
-print (novalista)
+print(multiplicaLista(lista))
